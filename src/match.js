f6a0626f45f8c6a7d3df2d5b0b171110a01aba9f
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-10-30 18:45:16 -0700

    from buremba

--- src/match.js
+++ src/match.js
@@ -7,13 +7,13 @@
 import { escapeValue } from './utilities'
 
 const defaultIgnore = {
-  attribute (attributeName) {
-    return [
-      'style',
-      'data-reactid',
-      'data-react-checksum'
-    ].indexOf(attributeName) > -1
-  }
+    attribute (attributeName) {
+        return [
+            'style',
+            'data-reactid',
+            'data-react-checksum'
+        ].indexOf(attributeName) > -1
+    }
 }
 
 /**
@@ -25,83 +25,92 @@ const defaultIgnore = {
  */
 export default function match (node, options) {
 
+<<<<<<< HEAD
   const {
     root = window.top.document,
     skip = null,
     priority = ['id', 'class', 'href', 'src'],
     ignore = {}
   } = options
+=======
+    const {
+        root = document,
+        skip = null,
+        priority = ['id', 'class', 'href', 'src'],
+        ignore = {}
+    } = options
 
-  const path = []
-  var element = node
-  var length = path.length
-  var ignoreClass = false
+    const path = []
+    var element = node
+    var length = path.length
+    var ignoreClass = false
+>>>>>>> ed223b2... Update match.js
 
-  const skipCompare = skip && (Array.isArray(skip) ? skip : [skip]).map((entry) => {
-    if (typeof entry !== 'function') {
-      return (element) => element === entry
-    }
-    return entry
-  })
+    const skipCompare = skip && (Array.isArray(skip) ? skip : [skip]).map((entry) => {
+        if (typeof entry !== 'function') {
+            return (element) => element === entry
+        }
+        return entry
+    })
 
-  const skipChecks = (element) => {
-    return skip && skipCompare.some((compare) => compare(element))
-  }
-
-  Object.keys(ignore).forEach((type) => {
-    if (type === 'class') {
-      ignoreClass = true
-    }
-    var predicate = ignore[type]
-    if (typeof predicate === 'function') return
-    if (typeof predicate === 'number') {
-      predicate = predicate.toString()
-    }
-    if (typeof predicate === 'string') {
-      predicate = new RegExp(escapeValue(predicate).replace(/\\/g, '\\\\'))
-    }
-    if (typeof predicate === 'boolean') {
-      predicate = predicate ? /(?:)/ : /.^/
-    }
-    // check class-/attributename for regex
-    ignore[type] = (name, value) => predicate.test(value)
-  })
-
-  if (ignoreClass) {
-    const ignoreAttribute = ignore.attribute
-    ignore.attribute = (name, value, defaultPredicate) => {
-      return ignore.class(value) || ignoreAttribute && ignoreAttribute(name, value, defaultPredicate)
-    }
-  }
-
-  while (element !== root) {
-    if (skipChecks(element) !== true) {
-      // ~ global
-      if (checkAttributes(priority, element, ignore, path, root)) break
-      if (checkTag(element, ignore, path, root)) break
-
-      // ~ local
-      checkAttributes(priority, element, ignore, path)
-      if (path.length === length) {
-        checkTag(element, ignore, path)
-      }
-
-      // define only one part each iteration
-      if (path.length === length) {
-        checkChilds(priority, element, ignore, path)
-      }
+    const skipChecks = (element) => {
+        return skip && skipCompare.some((compare) => compare(element))
     }
 
-    element = element.parentNode
-    length = path.length
-  }
+    Object.keys(ignore).forEach((type) => {
+        if (type === 'class') {
+            ignoreClass = true
+        }
+        var predicate = ignore[type]
+        if (typeof predicate === 'function') return
+        if (typeof predicate === 'number') {
+            predicate = predicate.toString()
+        }
+        if (typeof predicate === 'string') {
+            predicate = new RegExp(escapeValue(predicate).replace(/\\/g, '\\\\'))
+        }
+        if (typeof predicate === 'boolean') {
+            predicate = predicate ? /(?:)/ : /.^/
+        }
+        // check class-/attributename for regex
+        ignore[type] = (name, value) => predicate.test(value)
+    })
 
-  if (element === root) {
-    const pattern = findPattern(priority, element, ignore)
-    path.unshift(pattern)
-  }
+    if (ignoreClass) {
+        const ignoreAttribute = ignore.attribute
+        ignore.attribute = (name, value, defaultPredicate) => {
+            return ignore.class(value) || ignoreAttribute && ignoreAttribute(name, value, defaultPredicate)
+        }
+    }
 
-  return path.join(' ')
+    while (element !== root) {
+        if (skipChecks(element) !== true) {
+            // ~ global
+            if (checkAttributes(priority, element, ignore, path, root)) break
+            if (checkTag(element, ignore, path, root)) break
+
+            // ~ local
+            checkAttributes(priority, element, ignore, path)
+            if (path.length === length) {
+                checkTag(element, ignore, path)
+            }
+
+            // define only one part each iteration
+            if (path.length === length) {
+                checkChilds(priority, element, ignore, path)
+            }
+        }
+
+        element = element.parentNode
+        length = path.length
+    }
+
+    if (element === root) {
+        const pattern = findPattern(priority, element, ignore)
+        path.unshift(pattern)
+    }
+
+    return path.join(' ')
 }
 
 /**
@@ -115,15 +124,15 @@ export default function match (node, options) {
  * @return {boolean}                 - [description]
  */
 function checkAttributes (priority, element, ignore, path, parent = element.parentNode) {
-  const pattern = findAttributesPattern(priority, element, ignore)
-  if (pattern) {
-    const matches = parent.querySelectorAll(pattern)
-    if (matches.length === 1) {
-      path.unshift(pattern)
-      return true
+    const pattern = findAttributesPattern(priority, element, ignore)
+    if (pattern) {
+        const matches = parent.querySelectorAll(pattern)
+        if (matches.length === 1) {
+            path.unshift(pattern)
+            return true
+        }
     }
-  }
-  return false
+    return false
 }
 
 /**
@@ -135,54 +144,54 @@ function checkAttributes (priority, element, ignore, path, parent = element.pare
  * @return {string?}                 - [description]
  */
 function findAttributesPattern (priority, element, ignore) {
-  const attributes = element.attributes
-  const sortedKeys = Object.keys(attributes).sort((curr, next) => {
-    const currPos = priority.indexOf(attributes[curr].name)
-    const nextPos = priority.indexOf(attributes[next].name)
-    if (nextPos === -1) {
-      if (currPos === -1) {
-        return 0
-      }
-      return -1
-    }
-    return currPos - nextPos
-  })
-
-  for (var i = 0, l = sortedKeys.length; i < l; i++) {
-    const key = sortedKeys[i]
-    const attribute = attributes[key]
-    const attributeName = attribute.name
-    const attributeValue = escapeValue(attribute.value)
-
-    const currentIgnore = ignore[attributeName] || ignore.attribute
-    const currentIgnore = ignore[attributeName] || ignore.attribute
-    const currentDefaultIgnore = defaultIgnore[attributeName] || defaultIgnore.attribute
-    if (checkIgnore(currentIgnore, attributeName, attributeValue, currentDefaultIgnore)) {
-      continue
-    }
-
-    var pattern = `[${attributeName}="${attributeValue}"]`
-
-    if ((/\b\d/).test(attributeValue) === false) {
-      if (attributeName === 'id') {
-        pattern = `#${attributeValue}`
-      }
-
-      if (attributeName === 'class') {
-        var classes = attributeValue.trim();
-        const className;
-        if(ignore.ignoreClass) {
-           className = ignore.ignoreClass.split(/\s+/g).filter(ignore.ignoreClass).join('.')
-        } else {
-           className = attributeValue.trim().replace(/\s+/g, '.')
+    const attributes = element.attributes
+    const sortedKeys = Object.keys(attributes).sort((curr, next) => {
+        const currPos = priority.indexOf(attributes[curr].name)
+        const nextPos = priority.indexOf(attributes[next].name)
+        if (nextPos === -1) {
+            if (currPos === -1) {
+                return 0
+            }
+            return -1
         }
-        pattern = `.${className}`
-      }
-    }
+        return currPos - nextPos
+    })
 
-    return pattern
-  }
-  return null
+    for (var i = 0, l = sortedKeys.length; i < l; i++) {
+        const key = sortedKeys[i]
+        const attribute = attributes[key]
+        const attributeName = attribute.name
+        const attributeValue = escapeValue(attribute.value)
+
+        const currentIgnore = ignore[attributeName] || ignore.attribute
+        const currentIgnore = ignore[attributeName] || ignore.attribute
+        const currentDefaultIgnore = defaultIgnore[attributeName] || defaultIgnore.attribute
+        if (checkIgnore(currentIgnore, attributeName, attributeValue, currentDefaultIgnore)) {
+            continue
+        }
+
+        var pattern = `[${attributeName}="${attributeValue}"]`
+
+        if ((/\b\d/).test(attributeValue) === false) {
+            if (attributeName === 'id') {
+                pattern = `#${attributeValue}`
+            }
+
+            if (attributeName === 'class') {
+                const classes = attributeValue.trim();
+                var className;
+                if(ignore.ignoreClass) {
+                    className = classes.split(/\s+/g).filter(ignore.ignoreClass).join('.')
+                } else {
+                    className = classes.replace(/\s+/g, '.')
+                }
+                pattern = null
+            }
+        }
+
+        return pattern
+    }
+    return null
 }
 
 /**
@@ -195,15 +204,15 @@ function findAttributesPattern (priority, element, ignore) {
  * @return {boolean}                - [description]
  */
 function checkTag (element, ignore, path, parent = element.parentNode) {
-  const pattern = findTagPattern(element, ignore)
-  if (pattern) {
-    const matches = parent.getElementsByTagName(pattern)
-    if (matches.length === 1) {
-      path.unshift(pattern)
-      return true
+    const pattern = findTagPattern(element, ignore)
+    if (pattern) {
+        const matches = parent.getElementsByTagName(pattern)
+        if (matches.length === 1) {
+            path.unshift(pattern)
+            return true
+        }
     }
-  }
-  return false
+    return false
 }
 
 /**
@@ -214,11 +223,11 @@ function checkTag (element, ignore, path, parent = element.parentNode) {
  * @return {boolean}             - [description]
  */
 function findTagPattern (element, ignore) {
-  const tagName = element.tagName.toLowerCase()
-  if (checkIgnore(ignore.tag, null, tagName)) {
-    return null
-  }
-  return tagName
+    const tagName = element.tagName.toLowerCase()
+    if (checkIgnore(ignore.tag, null, tagName)) {
+        return null
+    }
+    return tagName
 }
 
 /**
@@ -233,23 +242,23 @@ function findTagPattern (element, ignore) {
  * @return {boolean}                 - [description]
  */
 function checkChilds (priority, element, ignore, path) {
-  const parent = element.parentNode
-  const children = parent.childTags || parent.children
-  for (var i = 0, l = children.length; i < l; i++) {
-    const child = children[i]
-    if (child === element) {
-      const childPattern = findPattern(priority, child, ignore)
-      if (!childPattern) {
-        return console.warn(`
+    const parent = element.parentNode
+    const children = parent.childTags || parent.children
+    for (var i = 0, l = children.length; i < l; i++) {
+        const child = children[i]
+        if (child === element) {
+            const childPattern = findPattern(priority, child, ignore)
+            if (!childPattern) {
+                return console.warn(`
           Element couldn\'t be matched through strict ignore pattern!
         `, child, ignore, childPattern)
-      }
-      const pattern = `> ${childPattern}:nth-child(${i+1})`
-      path.unshift(pattern)
-      return true
+            }
+            const pattern = `> ${childPattern}:nth-child(${i+1})`
+            path.unshift(pattern)
+            return true
+        }
     }
-  }
-  return false
+    return false
 }
 
 /**
@@ -261,11 +270,11 @@ function checkChilds (priority, element, ignore, path) {
  * @return {string}                  - [description]
  */
 function findPattern (priority, element, ignore) {
-  var pattern = findAttributesPattern(priority, element, ignore)
-  if (!pattern) {
-    pattern = findTagPattern(element, ignore)
-  }
-  return pattern
+    var pattern = findAttributesPattern(priority, element, ignore)
+    if (!pattern) {
+        pattern = findTagPattern(element, ignore)
+    }
+    return pattern
 }
 
 /**
@@ -278,12 +287,12 @@ function findPattern (priority, element, ignore) {
  * @return {boolean}                   - [description]
  */
 function checkIgnore (predicate, name, value, defaultPredicate) {
-  if (!value) {
-    return true
-  }
-  const check = predicate || defaultPredicate
-  if (!check) {
-    return false
-  }
-  return check(name, value, defaultPredicate)
+    if (!value) {
+        return true
+    }
+    const check = predicate || defaultPredicate
+    if (!check) {
+        return false
+    }
+    return check(name, value, defaultPredicate)
 }
